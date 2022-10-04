873ca4ba8eed601de7fc1ad4937559f33060290c
Justin Wong <jusw85@hotmail.com>
2022-10-05 00:01:46 +0800

    Update

--- events.gd
+++ events.gd
@@ -5,3 +5,5 @@ extends Node
 signal stage_exited
 signal stage_exited2
 signal upgrade_exited
+
+signal true_end
--- main/main.gd
+++ main/main.gd
@@ -24,12 +24,13 @@ func _ready():
 	Events.connect("stage_exited", self, "_on_stage_exit")
 	Events.connect("stage_exited2", self, "_on_stage_exit2")
 	Events.connect("upgrade_exited", self, "_on_upgrade_exit")
+	Events.connect("true_end", self, "_true_end")
 
-	player.walk_speed = 1000
-	player.power = 1200
-	player.gems = 1000
-	player.lantern = true
-	timer.wait_time = 1000
+#	player.walk_speed = 1000
+#	player.power = 1200
+#	player.gems = 1000
+#	player.lantern = true
+#	timer.wait_time = 1000
 
 
 func _on_Timer_timeout():
@@ -93,8 +94,8 @@ func _on_upgrade_exit():
 #	timer_label.text = "%.2f" % timer.wait_time
 
 	var new_stage
-#	if _stage_number % 5 == 0:
-	if _stage_number % 2 == 0:
+	if _stage_number % 5 == 0:
+#	if _stage_number % 2 == 0:
 		new_stage = LevelUpgrade.instance()
 	else:
 		var new_stage_num = ((_prev_stage_num + 1) + (randi() % (Levels.size() - 1))) % Levels.size()
@@ -123,12 +124,42 @@ const Levels = [
 	preload("res://stages/stage11.tscn"),
 	preload("res://stages/stage12.tscn"),
 ]
+
 #const Levels = [
 #	preload("res://stages/stage1.tscn"),
 #	preload("res://stages/stage12.tscn"),
 #]
 
 const LevelUpgrade = preload("res://stages/stage_upgrade.tscn")
+const Level_End = preload("res://end/stage_end.tscn")
 
 func _on_stage_exit2():
-	pass
+	player.position = Vector2(-1000, -1000)
+	yield(get_tree(), "idle_frame")
+	stage.get_child(0).connect("tree_exited", self, "_on_stage_exit3")
+	stage.get_child(0).queue_free()
+
+func _on_stage_exit3():
+	player.paused = false
+	player.visible = true
+
+	timer_label.visible = true
+	timer.start()
+#	timer.stop()
+#	timer_label.text = "%.2f" % timer.wait_time
+
+	var new_stage = Level_End.instance()
+#	new_stage = Levels[1].instance()
+	new_stage.difficulty_modifier = _stage_number - 1
+	stage.add_child(new_stage)
+	var spawn = get_tree().get_nodes_in_group("spawn")[0]
+	player.position = spawn.position
+
+
+func _true_end():
+	var end = load("res://end/true_end.tscn").instance()
+	get_tree().get_root().add_child(end)
+#	var label = end.get_node("Label")
+#	label.text = label.text % _stage_number
+	get_tree().current_scene = end
+	queue_free()
